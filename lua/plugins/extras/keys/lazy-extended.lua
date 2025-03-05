a8b6826867be380ae351d8d5000ef665df442d61
Frestein <frestein@tuta.io>
2025-03-06 02:24:00 +0500

    feat(lazy-exteded): add more keymaps, update hi

--- lua/plugins/extras/keys/lazy-extended.lua
+++ lua/plugins/extras/keys/lazy-extended.lua
@@ -1,7 +1,11 @@
 local map = vim.keymap.set
 
-map("n", "<leader>ll", "<cmd>Lazy home<cr>", { desc = "Home" })
+map("n", "<leader>lh", "<cmd>Lazy home<cr>", { desc = "Home" })
+map("n", "<leader>lH", "<cmd>Lazy health<cr>", { desc = "Health" })
+map("n", "<leader>ll", "<cmd>Lazy log<cr>", { desc = "Log" })
+map("n", "<leader>ld", "<cmd>Lazy debug<cr>", { desc = "Debug" })
 map("n", "<leader>ls", "<cmd>Lazy sync<cr>", { desc = "Sync" })
+map("n", "<leader>lc", "<cmd>Lazy check<cr>", { desc = "Check" })
 map("n", "<leader>lp", "<cmd>Lazy profile<cr>", { desc = "Profile" })
 map("n", "<leader>lx", "<cmd>LazyExtra<cr>", { desc = "Extra" })
 
@@ -10,5 +14,12 @@ return {
 	optional = true,
 	opts = function(_, opts)
 		table.insert(opts.spec, { "<leader>l", group = "lazy" })
+	end,
+	init = function()
+		local wk = require "which-key"
+		wk.add {
+			{ "<leader>lp", icon = { cat = "filetype", name = "lazy" } },
+			{ "<leader>ld", icon = { cat = "filetype", name = "lazy" } },
+		}
 	end,
 }
