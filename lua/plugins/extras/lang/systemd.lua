3db65063ff82302588f4b5b5b7749a7e534875ad
Frestein <frestein@tuta.io>
2025-03-07 01:31:53 +0500

    feat(lang/systemd): add systemd-language-server

--- lua/plugins/extras/lang/systemd.lua
+++ lua/plugins/extras/lang/systemd.lua
@@ -1,4 +1,41 @@
+local configs = require "lspconfig.configs"
+
+if not configs.systemd_ls then
+	configs.systemd_ls = {
+		default_config = {
+			cmd = { "systemd-language-server" },
+			filetypes = { "systemd" },
+			root_dir = function(fname)
+				return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
+			end,
+			single_file_support = true,
+		},
+		docs = {
+			description = [[
+https://github.com/psacawa/systemd-language-server
+`systemd-language-server` can be installed via `pip`:
+```sh
+pip install systemd-language-server
+```
+Language Server for Systemd unit files
+]],
+		},
+	}
+end
+
 return {
+	{
+		"neovim/nvim-lspconfig",
+		optional = true,
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			setup = {
+				systemd_ls = {},
+			},
+		},
+	},
+
 	{
 		"mfussenegger/nvim-lint",
 		optional = true,
