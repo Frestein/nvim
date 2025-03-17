8e833f6c90a0b2a360c91743e5700d5d698901d4
Frestein <frestein@tuta.io>
2025-03-17 07:55:40 +0500

    refactor: rewrite opts

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,63 @@
 local g = vim.g
 local o = vim.o
 
+-- Disable default providers
+g.loaded_node_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
+
+--- LazyVim  ---
 g.lazyvim_python_lsp = "basedpyright"
 
+--- UI ---
+o.numberwidth = 2
+o.pumblend = 15
+o.scrolloff = 5
+o.signcolumn = "yes"
+o.guicursor = table.concat({
+	"n-v-c:block",
+	"i-ci-ve:ver25",
+	"r-cr:hor20",
+	"o:hor50",
+	"a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
+	"sm:block-blinkwait175-blinkoff150-blinkon175",
+}, ",")
+o.cursorlineopt = "both"
+o.fillchars = table.concat({
+	"foldopen:",
+	"foldclose:",
+	"fold: ",
+	"foldsep: ", -- or "│" to use bar for show fold area
+	"diff:╱",
+	"eob: ",
+}, ",")
+
+--- Indents, Spaces, Tabulation ---
+o.shiftwidth = 4
+o.tabstop = 4
+o.softtabstop = 4
+
+--- Search ---
+o.ignorecase = true
+o.hlsearch = true
+o.infercase = true
+
+--- Folds ---
+o.foldlevelstart = 99
+o.foldlevel = 99
+o.foldmethod = "expr"
+o.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldtext = require "modules.foldtext"
+
+--- Swap ---
+o.swapfile = false
+
+--- Spell Checking ---
+o.spelllang = "en_us,ru_ru"
+o.spellfile = os.getenv "XDG_CONFIG_HOME" .. "/nvim/spell/en.utf-8.add"
+
+--- Langmapper ---
 local function escape(str)
 	local escape_chars = [[;,."|\]]
 	return vim.fn.escape(str, escape_chars)
@@ -12,83 +67,16 @@ local en = [[qwertyuiop[]asdfghjkl;zxcvbnm,.]]
 local ru = [[йцукенгшщзхъфывапролджячсмитьбю]]
 local en_shift = [[QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>]]
 local ru_shift = [[ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЯЧСМИТЬБЮ]]
-local langmap = vim.fn.join({
+
+o.langmap = vim.fn.join({
 	escape(ru_shift) .. ";" .. escape(en_shift),
 	escape(ru) .. ";" .. escape(en),
 }, ",")
 
-local options = {
-	--- UI ---
-	numberwidth = 2,
-	pumblend = 15,
-	scrolloff = 5,
-	signcolumn = "yes",
-	guicursor = table.concat({
-		"n-v-c:block",
-		"i-ci-ve:ver25",
-		"r-cr:hor20",
-		"o:hor50",
-		"a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
-		"sm:block-blinkwait175-blinkoff150-blinkon175",
-	}, ","),
-	cursorlineopt = "both",
-	fillchars = table.concat({
-		"foldopen:",
-		"foldclose:",
-		"fold: ",
-		"foldsep: ", -- or "│" to use bar for show fold area
-		"diff:╱",
-		"eob: ",
-	}, ","),
-
-	--- Indents, Spaces, Tabulation ---
-	shiftwidth = 4,
-	tabstop = 4,
-	softtabstop = 4,
-
-	--- Search ---
-	ignorecase = true,
-	hlsearch = true,
-	infercase = true,
-
-	--- Folds ---
-	foldlevelstart = 99,
-	foldlevel = 99,
-	foldmethod = "expr",
-	foldexpr = "nvim_treesitter#foldexpr()",
-	foldtext = require "modules.foldtext",
-
-	--- Swap ---
-	swapfile = false,
-
-	--- Spell Checking ---
-	spelllang = "en_us,ru_ru",
-	spellfile = os.getenv "HOME" .. "/.config/nvim/spell/en.utf-8.add",
-
-	--- Other ---
-	langmap = langmap,
-	wrap = true,
-
-	-- Disable nvim intro
-	shortmess = o.shortmess .. "sI",
-
-	-- Go to previous/next line with h,l,left arrow and right arrow
-	-- when cursor reaches end/beginning of line
-	whichwrap = o.whichwrap .. "<,>,[,],h,l",
-}
-
--- Disable default providers
-g.loaded_node_provider = 0
-g.loaded_python3_provider = 0
-g.loaded_perl_provider = 0
-g.loaded_ruby_provider = 0
-
-for option_name, value in pairs(options) do
-	-- To avoid errors on toggle nvim version
-	local ok, _ = pcall(vim.api.nvim_get_option_info2, option_name, {})
-	if ok then
-		o[option_name] = value
-	else
-		vim.notify("Option " .. option_name .. " is not supported", vim.log.levels.WARN)
-	end
-end
+--- Other ---
+o.wrap = true
+-- Disable nvim intro
+o.shortmess = o.shortmess .. "sI"
+-- Go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+o.whichwrap = o.whichwrap .. "<,>,[,],h,l"
