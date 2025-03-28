4d5681ed163685285832c30bb297c913ebec8e68
Frestein <frestein@tuta.io>
2025-03-28 11:15:01 +0500

    refactor(systemd): clean up

--- lua/plugins/extras/lang/systemd.lua
+++ lua/plugins/extras/lang/systemd.lua
@@ -14,34 +14,6 @@ return {
 			servers = {
 				systemd_ls = {},
 			},
-			setup = {
-				systemd_ls = function()
-					local configs = require "lspconfig.configs"
-
-					if not configs.systemd_ls then
-						configs.systemd_ls = {
-							default_config = {
-								cmd = { "systemd-language-server" },
-								filetypes = { "systemd" },
-								root_dir = function(fname)
-									return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
-								end,
-								single_file_support = true,
-							},
-							docs = {
-								description = [[
-https://github.com/psacawa/systemd-language-server
-`systemd-language-server` can be installed via `pip`:
-```sh
-pip install systemd-language-server
-```
-Language Server for Systemd unit files
-]],
-							},
-						}
-					end
-				end,
-			},
 		},
 	},
 
